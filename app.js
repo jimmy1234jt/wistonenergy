215afb7125586c2db1259a5706cbb84f610701aa
jimmy1234jt <ejeroodafewomoni@gmail.com>
2022-11-25 03:44:41 +0100

    slideshow and column cards

--- app.js
+++ app.js
@@ -12,3 +12,13 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 8000); // Change image every 2 seconds
 }
+
+// Hide all elements with class="containerTab", except for the one that matches the clickable grid column
+function openTab(tabName) {
+  var i, x;
+  x = document.getElementsByClassName("containerTab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(tabName).style.display = "block";
+}
